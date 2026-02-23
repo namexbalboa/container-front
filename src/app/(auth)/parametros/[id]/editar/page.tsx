887e41c49cf5bf73fed3513365473f3ea7a7b1fa
namexbalboa <naama_ortiz@hotmail.com>
2026-02-23 11:26:47 -0300

    fix: params

--- src/app/(auth)/parametros/[id]/editar/page.tsx
+++ src/app/(auth)/parametros/[id]/editar/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { use } from "react";
 import { ParametroForm } from "../../components/ParametroForm";
 
 export default function EditarParametroPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }) {
-  const { id } = use(params);
-
-  return <ParametroForm parametroId={parseInt(id)} />;
+  return <ParametroForm parametroId={parseInt(params.id)} />;
 }
